89685631134a46b0595f24b8ea8f0dc4178f3eac
spirit01 <phrozkova@localhost.localdomain>
2017-05-13 22:58:01 +0200

    upraveno pep8, neřešen řádek30, 54 a 103 -> pouze příliš dlouhý. Neřeším...

--- test_caxs.py
+++ test_caxs.py
@@ -51,9 +51,8 @@ quit
            s3=os.path.splitext(structure_1)[0],
            s4=os.path.splitext(structure_2)[0]))
 
-    out_pymol = subprocess.check_output("pymol -c file_for_pymol.pml | grep selection",
-     shell=True)
-    rmsd = float (out_pymol[out_pymol.index(b'=')+1:len(out_pymol)-1])
+    out_pymol = subprocess.check_output("pymol -c file_for_pymol.pml | grep selection", shell=True)
+    rmsd = float(out_pymol[out_pymol.index(b'=')+1:len(out_pymol)-1])
     print('RMSD ', structure_1, ' and ', structure_2, ' = ', rmsd)
     return rmsd
 
@@ -80,58 +79,55 @@ if args.mixing_koeficient != 1:
 
 print('Files from directory', args.myDirVariable)
 print('The number of the selected files',
-args.number_of_selected_files)
+      args.number_of_selected_files)
 print('The number of selected options', args.k_number_of_options)
-print('All pdb.dat files \n',pdb_files)
+print('All pdb.dat files \n', pdb_files)
 
-selected_files_for_ensamble = random.sample(pdb_files, args.number_of_selected_files)
+selected_files_for_ensamble = random.sample(pdb_files,
+                                            args.number_of_selected_files)
 print('Randomly selected files: \n', selected_files_for_ensamble)
 
-list_of_random_items = random.sample(selected_files_for_ensamble, args.k_number_of_options)
+list_of_random_items = random.sample(selected_files_for_ensamble,
+                                     args.k_number_of_options)
 print('Randomly selected files: \n', list_of_random_items)
 
-
-str1 = ''.join(str(e)+"\n"  for e in list_of_random_items)
+str1 = ''.join(str(e)+"\n" for e in list_of_random_items)
 
 for e in list_of_random_items:
     value_of_index = selected_files_for_ensamble.index(e)
     print(selected_files_for_ensamble.index(e))
 
-with open ("input_for_ensamble_fit", "w") as f:
-#f = open('input_for_ensamble_fit', 'w')
+with open("input_for_ensamble_fit", "w") as f:
     f.write(str1)
-    #f.close()
 
+# command = "/storage/brno3-cerit/home/krab1k/saxs-ensamble-fit/core/ensamble-fit -L -p /storage/brno2/home/petrahrozkova/SAXS/mod -n 10 -m /storage/brno2/home/petrahrozkova/SAXS/"+ list_of_random_items[0]
 
-#command = "/storage/brno3-cerit/home/krab1k/saxs-ensamble-fit/core/ensamble-fit -L -p /storage/brno2/home/petrahrozkova/SAXS/mod -n 10 -m /storage/brno2/home/petrahrozkova/SAXS/"+ list_of_random_items[0]
+# subprocess.call(command,shell=True)
 
-#subprocess.call(command,shell=True)
-
-#RMSD in PyMol
-with open('result','r') as f:
+# RMSD in PyMol
+with open('result', 'r') as f:
     (f.readline())
-    result=f.readline()
-    values_of_index_result=result.split(',')[4:]
-    str2 = ''.join(str(e)+"\n"  for e in values_of_index_result)
+    result = f.readline()
+    values_of_index_result = result.split(',')[4:]
+    str2 = ''.join(str(e)+"\n" for e in values_of_index_result)
     print(str2)
-    sum_rmsd=0
-#subprocess.call("module add pymol")
+    sum_rmsd = 0
+# subprocess.call("module add pymol")
 
-dictionary_index_and_structure=dict()
+dictionary_index_and_structure = dict()
 for i, j in enumerate(selected_files_for_ensamble):
-    dictionary_index_and_structure[i]=j
+    dictionary_index_and_structure[i] = j
 print(dictionary_index_and_structure)
 
 for i, j in enumerate(values_of_index_result):
-    print (i,j)
+    print(i, j)
     f = float(j)
     if f != 0:
         print('hodnota indexu', i)
-        print('číslo',i)
-        computed_rmsd=rmsd_pymol(selected_files_for_ensamble[i],list_of_random_items[0])
+        print('číslo', i)
+        computed_rmsd = rmsd_pymol(selected_files_for_ensamble[i],
+                                   list_of_random_items[0])
         print('Adjusted rmsd ', f*computed_rmsd, '\n')
         sum_rmsd += f*computed_rmsd
 
-print('Sum of RMSD',sum_rmsd)
-
-#print (str2)
+print('Sum of RMSD', sum_rmsd)
